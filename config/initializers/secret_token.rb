7a42ecd0bd0c719f50f7df69e01dd7f6ce109f10
speric <eric@prudentiadigital.com>
2012-10-05 17:38:52 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChargifyTodoList::Application.config.secret_token = '7ce29a15312fe829face8e1e7e67eaf6a30c1c42ce581dd707d37c432811af1d71b06a689b9d312daea5b01ffbb275d1ffd5cf3c9ec236705ae1864488e3a3b9'
