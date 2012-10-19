67a9ed2c62ba0f971af228102313918ae9cdb3da
speric <eric@prudentiadigital.com>
2012-10-19 00:59:06 -0400

    Some functional and unit tests.

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup :activate_authlogic
+  
+  test "accessing users#new while logged in redirects to items#index" do
+    UserSession.create(users(:eric))
+    get :new
+    assert_redirected_to items_path
+    assert_equal "You must be logged out to access this page", flash[:notice]
+  end
+
+  test "accessing users#new while logged out renders page" do
+    get :new
+    assert assigns(:user)
+    assert_template "users/new"
+  end
+
+  test "accessing users#create while logged in redirects to items#index" do
+    UserSession.create(users(:eric))
+    post :create
+    assert_redirected_to items_path
+    assert_equal "You must be logged out to access this page", flash[:notice]
+  end
+
+  test "create user with existing email render user#new" do
+    post :create, :user => { :email_address => users(:eric).email_address }
+    assert_template "users/new"
+    assert_select "h4.alert-heading", "There were some errors with your request:"
+  end
+
+  test "create valid user saves and redirects items#new" do
+    post :create, :user => { :email_address => "eric@prudentiadigital.com" }
+    assert_redirected_to items_path
+    assert_equal "Account registered!", flash[:notice]
+  end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,15 +1,27 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-   test "can't create user with invalid email address" do
-     user = User.new
-     user.email_address = "speric"
-     assert !user.save
-   end
+  test "can't create a user with no email address" do
+    user = User.new
+    user.email_address = ""
+    assert !user.save
+  end
 
-   test "can create user with valid email address" do
-     user = User.new
-     user.email_address = "speric@gmail.com"
-     assert user.save
-   end
+  test "can't create user with invalid email address" do
+    user = User.new
+    user.email_address = "eric"
+    assert !user.save
+  end
+
+  test "can create user with valid email address" do
+    user = User.new
+    user.email_address = "eric@prudentiadigital.com"
+    assert user.save
+  end
+
+  test "can't create user whose email address already exists" do
+    user = User.new
+    user.email_address = "speric@gmail.com"
+    assert !user.save #user already in db via fixtures
+  end
 end
